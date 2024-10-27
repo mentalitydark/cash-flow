8b29af1b4d691d9dba2a6578de48376059b54a33
mentalitydark <guilherme.oliveira.01@outlook.com>
2024-10-27 10:16:30 -0300

    change class Database

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,43 +1,42 @@
-import { Database as _Database } from "sqlite3"
+import sqlite3 from "sqlite3"
+import { open, Database as _Database } from "sqlite"
 
 export abstract class Database {
 
     private static instance: _Database|null = null
 
-    public static getConnection(): _Database {
+    public static async get() {
         if (Database.instance) {
             return Database.instance
         }
 
-        Database.instance = new _Database("./cash-flow.db", function (error) {
-            if (error) {
-                throw error
-            }
-        })
+        try {
+            const connection = await open({
+                filename: "./cash-flow.db",
+                driver: sqlite3.Database
+            }) as _Database
 
-        return Database.instance
+            Database.instance = connection
+    
+            return Database.instance
+        } catch (error) {
+            throw error
+        }
     }
 
-    public static closeConnection(): boolean {
+    public static async close(): Promise<boolean> {
         if (!Database.instance) {
             return true
         }
 
-        Database.instance.close(function (error) {
-            if (error) {
-                console.error(error)
-            }
-        })
+        try {
+            await Database.instance.close()
+            Database.instance = null
+        } catch (error) {
+            throw error
+        }
 
         return true
-    }
-
-    public static getNewConnection(): _Database {
-        return new _Database("./cash-flow.db", function (error) {
-            if (error) {
-                throw error
-            }
-        })
     }
 
 }
