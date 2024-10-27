a705234ba2eb9049e2bbde0d6aa240b9447dc8d5
mentalitydark <guilherme.oliveira.01@outlook.com>
2024-10-27 10:15:23 -0300

    add return Promise<T>

--- src/Repositories/RepositoryBase.ts
+++ src/Repositories/RepositoryBase.ts
@@ -3,21 +3,21 @@ import { IGenericRepository } from "@/interfaces"
 
 export abstract class RepositoryBase<T extends GenericEntity<T>> implements IGenericRepository<T> {
 
-    public abstract getAll(): T[]
-    public abstract getOne(id: number): T
-    public abstract delete(entity: T): void
+    public abstract getAll(): T[]|Promise<T[]>
+    public abstract getOne(id: number): T|Promise<T>
+    public abstract delete(entity: T): void|Promise<void>
 
     private getCurrentTime() {
         return (new Date()).getTime()
     }
 
-    public insert(entity: T): T {
+    public insert(entity: T): T|Promise<T> {
         entity.created_at = this.getCurrentTime()
 
         return entity
     }
 
-    public update(entity: T): T {
+    public update(entity: T): T|Promise<T> {
         entity.updated_at = this.getCurrentTime()
 
         return entity
