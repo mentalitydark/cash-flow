f66bd497801353d9b3867b8562e4490aab394530
mentalitydark <guilherme.oliveira.01@outlook.com>
2024-10-27 20:30:29 -0300

    change code from routes

--- src/app/api/cash-flow/route.ts
+++ src/app/api/cash-flow/route.ts
@@ -4,7 +4,7 @@ import { ZodError } from "zod"
 import { RepositoryDatabase } from "@/Repositories/CashFlow"
 import { EntityNotFound } from "@/errors"
 import { CashFlow, Response } from "@/classes"
-import { ICashFlow } from "@/interfaces"
+import { ICashFlowProps } from "@/interfaces"
 
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url)
@@ -17,11 +17,11 @@ export async function GET(request: NextRequest) {
         if (id) {
             const entity = await repository.getOne(Number(id))
 
-            return Response.OK(entity.toJson())
+            return Response.OK(entity.toJSON())
         } else {
             const entities = await repository.getAll()
 
-            return Response.OK(entities.map(e => e.toJson()))
+            return Response.OK(entities.map(e => e.toJSON()))
         }
     } catch(error) {
         if (error instanceof EntityNotFound)
@@ -35,15 +35,17 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
     try {
-        const body = await request.json() as ICashFlow
+        const body = await request.json() as ICashFlowProps
 
-        const entity = CashFlow.createFromJson(CashFlow.validate(body))
+        const entity = new CashFlow()
+        
+        entity.createFromJSON(CashFlow.validate(body))
 
         const repository = new RepositoryDatabase
 
         await repository.insert(entity)
     
-        return Response.CREATED(entity.toJson())
+        return Response.CREATED(entity.toJSON())
     } catch (error) {
         if (error instanceof ZodError)
             return Response.BAD_REQUEST(error, { title: 'Parameters required', instance: request.url, error: error.issues})
